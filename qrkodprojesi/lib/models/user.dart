42970b65dcc1480099692179559546bfa5c5b774
Can Ali Sevin <sevinncan@gmail.com>
2025-12-19 20:34:54 +0300

    Remove unused firstName and lastName fields
    Removed commented-out firstName and lastName fields from User class.

--- qrkodprojesi/lib/models/user.dart
+++ qrkodprojesi/lib/models/user.dart
@@ -1,15 +1,13 @@
 // lib/models/user.dart (QR Kod Üretme Uygulaması İçin)
 class User {
   final int id;
-  // final String firstName; // <<< KALDIRILDI
-  // final String lastName; // <<< KALDIRILDI
+
   final String company;
   final String username;
 
   User({
     required this.id,
-    // required this.firstName, // <<< KALDIRILDI
-    // required this.lastName, // <<< KALDIRILDI
+  
     required this.company,
     required this.username,
   });
@@ -17,8 +15,7 @@ class User {
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'] as int,
-      // firstName: json['firstName'] as String, // <<< KALDIRILDI
-      // lastName: json['lastName'] as String, // <<< KALDIRILDI
+     
       company: json['company'] as String,
       username: json['username'] as String,
     );
@@ -27,8 +24,7 @@ class User {
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      // 'firstName': firstName, // <<< KALDIRILDI
-      // 'lastName': lastName, // <<< KALDIRILDI
+   
       'company': company,
       'username': username,
     };
